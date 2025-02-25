d57da129421fb7cec634cc54d2b93dabd70409a5
UP24028 <up240028@alumnos.upa.edu.mx>
2025-02-25 08:33:20 -0600

    HI

---
+++ Trabajo/Dia 3.py
@@ -0,0 +1,21 @@
+#1
+
+age= int(input("Escribe tu edad"))
+
+##2
+
+height=float(input("Escribe tu alrtura"))
+
+##3
+
+complex = complex(input("Un numero complejo"))
+
+##4 triangle
+
+print("")
+base = int(input("Ingresa la base del triangulo"))
+heightT= int(input("Ingresa la altura del triangulo"))
+area= float(( base * heightT)/2)
+print("E l area de un triangulo es: ", area )
+
+#5
