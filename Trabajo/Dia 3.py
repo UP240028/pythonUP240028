9b1c42f54b0c074d99d15b4c4e815ee2bfbb15c7
UP24028 <up240028@alumnos.upa.edu.mx>
2025-03-04 09:04:34 -0600

    por fin dia 3

--- Trabajo/Dia 3.py
+++ Trabajo/Dia 3.py
@@ -1,18 +1,23 @@
 #1
-
+print("")
+print("#Programa 1")
 age= int(input("Escribe tu edad: "))
 
 ##2
-
+print("")
+print("#Programa 2")
 height=float(input("Escribe tu alrtura: "))
 
 ##3
+print("")
+print("#Programa 3")
 
 complex = complex(input("Un numero complejo: "))
 
 ##4 triangle
 
 print("")
+print("#Programa 4")
 base = int(input("Ingresa la base del triangulo: "))
 heightT= int(input("Ingresa la altura del triangul: "))
 area= float(( base * heightT)/2)
@@ -21,7 +26,7 @@ print("E l area de un triangulo es: ", area )
 #5
 
 print("")
-
+print("#Programa 5")
 print("Perimetro de un triangulo")
 
 sideA= int(input("ingresa el lado A del triangulo: "))
@@ -33,7 +38,7 @@ print("El perimetro del triangulo es: ", perimeter)
 #6
 
 print("")
-
+print("#Programa 6")
 print("Area de un rectangulo")
 
 length= int(input("Ingresa el largo del rectangulo: "))
@@ -46,7 +51,7 @@ print("El perimetro del rectangulo es: ", perimeterRec)
 #7
 
 print("")
-
+print("#Programa 7")
 print("Area y circunferencia de un circulo")
 
 radius= int(input("Ingresa el radio del circulo: "))
@@ -59,7 +64,7 @@ print("La circunferencia del circulo es: ", circumference)
 #8
 
 print("")
-
+print("#Programa 8")
 print("Interseccion de la pendiente")
 pendiente= 2 #la pendiente de la ecuacion
 intY= -2 #Interaccion con ele Y
@@ -70,6 +75,7 @@ print("La interaccion es de: ", intX)
 #9
 
 print("")
+print("#Programa 9")
 ejeX1=2 #el valor de x1
 ejeY1=2 #el valor de y1
 ejeX2=6 #el valor de x2
@@ -82,13 +88,14 @@ print("La velocidad eclidiana es: ",velocidadEcl)
 #10 Compare the slopes
 
 print("")
+print("#Programa 10")
 print("Vamos a comparar pendientes")
 compare= pendiente >= pendienteM
 print("¿La copmparacion indica que la primer pendiente es mayor o igual que la segunda?: ", compare )
 
 #11 Calcular valor de Y
 
-print("")
+print("#Programa 11")
 X1=1
 Y1= (X1**2) + (6*X1) + 9
 print("cuando X vale 1 obtenemos que Y vale: ", Y1) 
@@ -107,7 +114,7 @@ print("Cuando X vale -3 obtenemos que Y vale: ", Y3)
 #12 Comparacion de titanes
 
 print("")
-
+print("#Programa 12")
 phyton= 2
 dragon= 3
 PhyandDragon= phyton>dragon
@@ -115,8 +122,8 @@ print(" ¿ES verdad que dragon es mas vergas que phyton? ")
 print(PhyandDragon)
 
 #13 
-
 print("")
+print("#Proframa 13")
 print("¿On esta en dragon o phyton?")
 dragono="on"
 phyton1= "luis"
@@ -127,7 +134,7 @@ else:
 
 #14
 
-print("")
+print("#Programa 14")
 print("¿esta jerga en la siguinte oracion?")
 print("espero que este curso no este lleno de jerga")
 sentence="espero que este curso no este lleno de jerga "
@@ -137,8 +144,8 @@ else:
     print( "No no esta jerga en la oracion")
 
 #15
-
 print("")
+print("#Programa 15")
 dragon15= "Peso pluma es el mas duro de mexico"
 phyton15= "El anciano ya no hace partidos en el ejido"
 print("Tengo dos oraciones, ¿Cual deice encendido?")
@@ -154,7 +161,7 @@ else:
 #16
 
 print("")
-
+print("#Programa 16")
 print("Longitud del texto de la variable phyton")
 phyton16="Esta clase dura una hora y he aventajado poco"
 print(len(phyton16))
@@ -163,6 +170,7 @@ print(len(phyton16))
 #17
 
 print("")
+print("#Programa 17")
 
 numero17=int(input("Ingresa un Numero entero para saber si es par: "))
 if numero17 % 2 == 0:
@@ -173,4 +181,47 @@ else:
 #18
 
 print("")
+print("#Programa 18")
+print("¡La division del piso de 7x3 es igual al valor init convertido de 2.7?")
 
+print(7//3 == int(2.7))
+
+#19
+print("")
+print("#Programa 19")
+print("¿El typo 10 es igual a 10?")
+print(type(10) == 10) 
+
+#20
+print("")
+print("#Programa 20")
+print("¿El entero de 9.8 es igual a 10?")
+print(int(9.8)==10)
+
+#21
+print("")
+print("#Programa 21")
+print("Calcula tu paga")
+horas=int(input("ingresa las horas trabajadas: "))
+tarifa=int(input("ingresa tu tarifa por hora: "))
+paga= (horas * tarifa)
+print("Tu paga es de : ",paga)
+
+#22
+print("")
+print("#Programa 23")
+print("Checa cuanto te queda de vida")
+añosactuales= int(input("Ingrese su edad actual: "))
+edadestablecida= 100
+añosrestantes= (edadestablecida -añosactuales)
+vidarestante= añosrestantes*365*24*3600
+print("tu vida restante es de: ", vidarestante )
+
+#23
+print("")
+print("#Programa 23")
+print("1 1 1 1 1")
+print("2 1 2 4 8")
+print("3 1 3 9 27")
+print("4 1 4 16 64")
+print(" 5 1 5 25")
