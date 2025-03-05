790c2b7eba063a41a96875c572ca9790637cb54d
UP24028 <up240028@alumnos.upa.edu.mx>
2025-03-05 10:06:29 -0600

    Dia 4

---
+++ Dia 4 .py
@@ -0,0 +1,32 @@
+#Programa 1
+print("")
+first_word= "Treinta"
+second_word= "Dias"
+thir_word="De"
+fourth_word= "Phyton"
+space=" "
+full_Sentence= first_word + space + second_word + space + thir_word + space + fourth_word
+print(full_Sentence)
+ 
+ #Programa 2
+print("")
+firstword= "Codificacion"
+secondword= "Para"
+thirdword="Todos"
+full_Sentence2= firstword + space + secondword + space + thirdword
+print(full_Sentence2)
+
+#Programa 4
+print("")
+company= "Codificacion para todos"
+
+#Programa 5 
+print("")
+print("Longitud de variable: ")
+print(len(company))
+
+#Programa 6
+print("")
+print("Cambiando caracteres a mayuscula: ")
+company1= company.upper()
+print(company1)
