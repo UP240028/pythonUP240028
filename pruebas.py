3b47f150530248c71bf01796016fcb78d14c7091
UP24028 <up240028@alumnos.upa.edu.mx>
2025-03-11 09:36:03 -0600

    Dia 6 1/2

--- pruebas.py
+++ pruebas.py
@@ -1,6 +1,4 @@
-it_companies = ["Google", "Apple", "Microsoft", "Amazon", "Tesla"]
-for i in range(len(it_companies)):
-    if it_companies[i] != "IBM":
-        it_companies[i] = it_companies[i].upper()
-        break  
-print("Updated list of IT companies:", it_companies)
+print("")
+Sisters=("Estrella", "Sarai")
+brothers=("Rafael", "Juanito")
+print(Sisters, brothers)
