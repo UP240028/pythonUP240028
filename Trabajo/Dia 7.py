882c14c89b20aab56936ef96f698ed9abafa2bd9
UP24028 <up240028@alumnos.upa.edu.mx>
2025-03-13 11:43:27 -0600

    13/03/2025

---
+++ Trabajo/Dia 7.py
@@ -0,0 +1,27 @@
+#ejercicio 1
+it_companies ={'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print(it_companies)
+print(len(it_companies))
+
+#ejercicio 2
+print("")
+it_companies.add("Twitter")
+print(it_companies)
+
+#ejercicio 3
+print("")
+it_companies.update(["Youtube","Tik tok"])
+print(it_companies)
+
+#ejrcicio 4
+print("")
+it_companies.remove("Microsoft")
+print(it_companies)
+
+#ejercicio 5
+print("")
+print("Diferencia entre remove y discard")
+print("La diferencia es de que el discard sirve para descargar un elemento por una vez y el remove lo elimina ")
+
+#ejercicio 6
+print("")
